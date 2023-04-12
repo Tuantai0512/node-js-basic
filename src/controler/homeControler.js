ad2674f6fccffb5c2958b73b74fbb46c4b24441d
dev-peter <tuantai0512@gmail.com>
2023-04-13 00:43:55 +0700

    Connect DB MySQL

--- src/controler/homeControler.js
+++ src/controler/homeControler.js
@@ -1,6 +1,16 @@
+import connection from "../configs/connectDB";
+
 let getHomepage = (req, res) => {
     //logic
-    return res.render('test/index.ejs');
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users`',
+        function (err, results, fields) {
+            data = results;
+            console.log(data); // results contains rows returned by server
+            return res.render('test/index.ejs', { dataUser: JSON.stringify(data) });
+        }
+    );
 }
 
 module.exports = {
