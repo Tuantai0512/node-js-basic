ad645c13abe7e86467edfb5bfc30b3bcc8518d76
dev-peter <tuantai0512@gmail.com>
2023-04-16 17:24:31 +0700

    finish CRUD

--- src/controler/homeControler.js
+++ src/controler/homeControler.js
@@ -8,7 +8,7 @@ let getHomepage = async (req, res) => {
 
 let getDetailPage = async (req, res) => {
     let id = req.params.userID;
-    let [user, fields] = await pool.execute(`SELECT * FROM users where id = ?`,[id]);
+    let [user] = await pool.execute(`SELECT * FROM users where id = ?`,[id]);
     return res.render('detail.ejs', { userDetail: user[0] });
 }
 
@@ -18,6 +18,25 @@ let createNewUser = async(req, res) => {
     return res.redirect('/');
 }
 
+let deleteUser = async(req, res) => {
+    let id = req.body.userID;
+    await pool.execute(`DELETE FROM users WHERE id = ?;`,[id]);
+    return res.redirect('/');
+}
+
+let editUser = async(req, res) => {
+    let id = req.params.userID;
+    let [user] = await pool.execute(`SELECT * FROM users where id = ?`,[id]);
+    return res.render('update.ejs', { userUpdate: user[0] });
+}
+
+let updateUser = async(req, res) => {
+    let { firstName, lastName, email, address, id } = req.body;
+    await pool.execute('update users set firstName= ?, lastName = ? , email = ? , address= ? where id = ?',
+        [firstName, lastName, email, address, id]);
+    return res.redirect('/');
+}
+
 module.exports = {
-    getHomepage, getDetailPage, createNewUser
+    getHomepage, getDetailPage, createNewUser, deleteUser, editUser, updateUser
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -5,7 +5,10 @@ let router = express.Router();
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
     router.get('/detail/users/:userID', homeController.getDetailPage);
-    router.post('/create-new-user', homeController.createNewUser)
+    router.post('/create-new-user', homeController.createNewUser);
+    router.post('/remove-user', homeController.deleteUser);
+    router.get('/edit-user/:userID', homeController.editUser);
+    router.post('/update-user/:userID', homeController.updateUser);
     router.get('/about', (req, res) => {
         res.send(`I'm Hậu tạ vcl`)
     })
